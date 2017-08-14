7ecbb3f4784f78c3b668b2edf7998bce6eb161dd
Eric R <n644dc@gmail.com>
2017-08-13 18:05:38 -0600

    updating tornado server and web client

---
+++ rover/server.py
@@ -0,0 +1,39 @@
+from tornado import websocket, web, ioloop
+import json
+
+cl = []
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+        self.sendMsg("Client Connected")
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+    def on_message(self, message):
+        print("Received:"+ message)
+
+    def sendMsg(self, message):
+         data = {"type": "server", "value" : message}
+         self.write_message(data)
+         
+        
+
+app = web.Application([
+    (r'/', IndexHandler),
+    (r'/ws', SocketHandler)
+])
+
+if __name__ == '__main__':
+    app.listen(8888)
+    ioloop.IOLoop.instance().start()
